a6cddf2156b2088f5e4d3ebbafb73869b87b0a14
unknown <jyek121242@gmail.com>
2025-05-13 20:32:22 +0900

    new

--- src/components/page/post/view/components/listMainLeft.component.tsx
+++ src/components/page/post/view/components/listMainLeft.component.tsx
@@ -1,17 +1,46 @@
-import styles from "./listMainLeft.module.scss";
+import styles from './listMainLeft.module.scss';
 import Image from 'next/image';
 
-// props 받기
-export default function ListMainLeft({ imageSrc }: { imageSrc: string }) {
+interface ListLeftProps {
+    imageSrc: string;
+    text: string;
+    date: string;
+    title: string;
+    link: string;
+}
+
+export default function ListMainLeft({ text, date, title, link, imageSrc }: ListLeftProps) {
     return (
-        <div className={styles.list_main_top}>
-            <h3>
-                <span className={styles.image_wrapper}>
-                    <Image src={imageSrc} alt="sample" fill />
-                </span>
-                <span></span>
-            </h3>
-            <p>2025년 04월 30일</p>
-        </div>
+        <>
+            <div className={styles.list_main_top}>
+                <h3>
+                    <span className={styles.image_wrapper}></span>
+                    <span>DEVOTEE 요약</span>
+                </h3>
+                <p className={styles.list_days}>{date}</p>
+            </div>
+            <div className={styles.list_main_middle}>
+                <p>{text}</p>
+            </div>
+            <div className={styles.list_main_bottom}>
+                <h3>본문보기</h3>
+                <div className={styles.list_main_more}>
+                    <a href={link}>
+                        <div className={styles.more_sum_main}>
+                            <div className={styles.more_sum}>
+                                <Image src={imageSrc} fill={true} alt="sample" />
+                            </div>
+                            <div className={styles.more_right}>
+                                <div className={styles.more_title}>{title}</div>
+                                <div className={styles.more_text}>
+                                    {text.length > 100 ? `${text.slice(0, 90)}...` : text}
+                                </div>
+                                <div className={styles.more_bottom}>더보기 →</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </>
     );
 }
--- src/components/page/post/view/postView.component.tsx
+++ src/components/page/post/view/postView.component.tsx
@@ -1,18 +1,28 @@
-import styles from "./postView.module.scss";
-import ListMainLeft from "./components/listMainLeft.component";
+import styles from './postView.module.scss';
+import ListMainLeft from './components/listMainLeft.component';
 
-export default function ViewComponent() {
+export default function ViewComponent({
+    title = 'Excited to share my latest tech project! #codinglife',
+    text = '명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 데 도움이 됩니다. 다양한 명상 기법 중에서도 호흡 명상과 마음 챙김 명상이 특히 효과적입니다. 또한, 명상은 자기 인식을 높이고 감정 조절 능력을 향상시켜 전반적인 정신 건강에 긍정적인 영향을 미칩니다. 일상 속에서 꾸준히 명상을 실천하면 심리적 안정과 행복을 증진할 수 있습니다.명상의 힘은 마음을 가라앉히고 내면의 평화를 찾는 방법을 제공합니다. 정기적인 명상은 스트레스를 줄이고 집중력을 향상시키는 데 도움이 됩니다. 다양한 명상 기법 중에서도 호흡 명상과 마음 챙김 명상이 특히 효과적입니다. 또한, 명상은 자기 인식을 높이고 감정 조절 능력을 향상시켜 전반적인 정신 건강에 긍정적인 영향을 미칩니다. 일상 속에서 꾸준히 명상을 실천하면 심리적 안정과 행복을 증진할 수 있습니다.',
+    date = '2025년 04월 30일',
+    link = '#',
+    imageSrc = '/images/logo.png',
+}) {
     return (
-        <div className={styles.inner}>
-            <h2 className={styles.main_h2}>
-                Excited to share my latest tech project! #codinglife
-            </h2>
+        <main className={styles.inner}>
+            <h2 className={styles.main_h2}>{title}</h2>
             <ul className={styles.write_list}>
                 <li>
-                    <ListMainLeft imageSrc="/images/view-robot.png" />
+                    <ListMainLeft
+                        text={text}
+                        date={date}
+                        title={title}
+                        link={link}
+                        imageSrc={imageSrc}
+                    />
                 </li>
                 <li></li>
             </ul>
-        </div>
+        </main>
     );
 }
