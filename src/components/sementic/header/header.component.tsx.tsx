0492d6bf7e3eccdf26276f904f1563eef442a5fd
hyunu <gustlr8399@naver.com>
2025-05-13 15:00:35 +0900

    ...

--- src/components/sementic/header/header.component.tsx.tsx
+++ src/components/sementic/header/header.component.tsx.tsx
@@ -1,8 +1,11 @@
 // 이 파일은 '/home' 경로의 페이지 컴포넌트를 렌더링합니다.
 // 실제 UI는 components/page/home 폴더에 있는 컴포넌트로 구성됩니다.
 
-import HomeComponent from "@components/page/home/home.component";
+import GnbComponent from "@components/sementic/header/components/gnb.components";
+import UserBtnComponent from "@components/sementic/header/components/userBtn.components";
 
 export default function HomePage() {
-  return <HomeComponent /> ;
+  return <GnbComponent /> ;
+  return <UserBtnComponent /> ;
 }
+
