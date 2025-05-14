9469e96b963fa9c37ee622c7610ceba93cff4973
unknown <jyek121242@gmail.com>
2025-05-14 14:51:14 +0900

    뷰퍼블리싱

--- src/components/page/post/view/components/listMainRight.component.tsx
+++ src/components/page/post/view/components/listMainRight.component.tsx
@@ -1,21 +1,55 @@
 import styles from './listMainRight.module.scss';
-import Image from 'next/image';
 
-interface ListLeftProps {}
+interface ListRightProps {
+    write: string;
+    views: number;
+    hearts: number;
+}
 
-export default function ListMainRight() {
+export default function ListMainRight({ write, views, hearts }: ListRightProps) {
     return (
         <>
             <div className={styles.right_card_box}>
-                <h3>
-                    <span className={styles.image_wrapper}></span>
-                    <span>DEVOTEE 요약</span>
-                </h3>
+                <div className={styles.main_right_card}>
+                    <h3>
+                        <span className={styles.img_box}>
+                            <i className={styles.writer_img}></i>
+                        </span>
+                        <span>{write}</span>
+                    </h3>
+                    <div className={styles.card_list}>
+                        <div className={styles.card_list_second}>
+                            <button>
+                                <span className={styles.heart_box}></span>
+                                <p>{hearts}</p>
+                            </button>
+                            <button>
+                                <span className={styles.view_box}></span>
+                                <p>{views}</p>
+                            </button>
+                        </div>
+                        <ul className={styles.navi_list}>
+                            <li>
+                                <a href="#">목록</a>
+                            </li>
+                            <li>
+                                <a href="#">이전글</a>
+                            </li>
+                            <li>
+                                <a href="#">다음글</a>
+                            </li>
+                        </ul>
+                        <div className={styles.share_btn}>
+                            <a href="#">
+                                <span className={styles.share_img}></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.list_main_middle}></div>
-            <div className={styles.list_main_bottom}>
-                <h3>본문보기</h3>
-                <div className={styles.list_main_more}></div>
+            <div className={styles.button_box}>
+                <button className={styles.edit_btn}>수정하기</button>
+                <button className={styles.delete_btn}>삭제하기</button>
             </div>
         </>
     );
