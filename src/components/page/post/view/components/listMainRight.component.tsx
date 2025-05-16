88b6392e598e4d3c9caa6c3501912ceafca9c6ed
unknown <jyek121242@gmail.com>
2025-05-16 11:28:24 +0900

    new

--- src/components/page/post/view/components/listMainRight.component.tsx
+++ src/components/page/post/view/components/listMainRight.component.tsx
@@ -30,6 +30,7 @@ export default function ListMainRight({ write, views, hearts, list, before, afte
             await navigator.clipboard.writeText(window.location.href);
             alert('복사되었습니다!');
         } catch (err) {
+            console.error('복사 실패:', err);
             alert('복사에 실패했습니다.');
         }
     };
