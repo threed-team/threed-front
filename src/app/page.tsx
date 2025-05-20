df7cef6f71f62dfd6c5f021864448e01940aad24
lEOOEE <ieunseo802@gmail.com>
2025-05-20 14:04:01 +0900

    .......

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@
   writer : 이은서
   src : http://localhost:3000/
 */
+import { useParams } from "next/navigation";
 import HeaderPageComponent from "@components/sementic/header/header.component.tsx";
-import HomeComponent from "@components/page/home/home.component";
+import HomeComponent from "@components/page/company/company.component";
 import FooterPageComponent from "@components/sementic/footer/footer.component";
 
 export default function Home() {
+  const params = useParams();
+  const type = params.type as "company" | "member";
+
   return (
     <>
       <HeaderPageComponent />
-      <HomeComponent />
+      <HomeComponent type={type} /> 
       <FooterPageComponent />
     </>
   )
-ㄴ
 }
