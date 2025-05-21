d366ca0dde981ac306a70bbce7630c8690ed664e
lEOOEE <ieunseo802@gmail.com>
2025-05-21 09:26:32 +0900

    user퀵매뉴 설정

--- src/components/page/user/UserState.tsx
+++ src/components/page/user/UserState.tsx
@@ -6,13 +6,13 @@ export default function UserStateomponent() {
     return (
         <div className={styles.user_state}>
             <div className={styles.state_logout}>
-                <Link href={'#'}>
+                <Link href={'/login'}>
                     <i className={styles.ico_logout}></i><span>로그아웃</span>
                 </Link>
-                
+
             </div>
             <div className={styles.state_mypage}>
-                <Link href={'#'}>
+                <Link href={'/mypage'}>
                     <i className={styles.ico_mypage}></i><span>마이페이지</span>
                 </Link>
             </div>
