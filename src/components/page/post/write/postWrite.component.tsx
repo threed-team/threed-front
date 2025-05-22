2e1cddc33d8b12449809f8dd064cb85624bba606
unknown <jyek121242@gmail.com>
2025-05-22 16:51:45 +0900

    라우팅추가

--- src/components/page/post/write/postWrite.component.tsx
+++ src/components/page/post/write/postWrite.component.tsx
@@ -5,10 +5,13 @@ import dynamic from 'next/dynamic';
 import HashtagInput from './components/hashTag.componant';
 import FieldSelector from './components/fileSelector.componant';
 import { usePostWrite } from './hooks/usePostWrite';
+import Loading from '@lib/loading/full.component'; // ✅ 로딩 컴포넌트 import
+import { useRouter } from 'next/navigation';
 
 const WriteContent = dynamic(() => import('./components/writeContent.component'), { ssr: false });
 
 export default function WriteComponent() {
+    const router = useRouter();
     const {
         postId,
         post,
@@ -21,14 +24,18 @@ export default function WriteComponent() {
         handleSubmit
     } = usePostWrite();
 
-    if (loading) return <p>불러오는 중입니다...</p>;
-    if (error) return <p>오류 발생: {String(error)}</p>;
+    if (!loading && error) {
+        router.replace('/');
+        return null;
+    }
+
+    if (loading) return <Loading />;
 
     return (
         <div className={styles.write_main}>
             <h2>
                 <span className={styles.img_box}></span>
-                <span>{postId ? '글 수정' : '새 글 작성'}</span>
+                <span>{postId === 1 ? '새 글 작성' : '글 수정'}</span>
             </h2>
             <form>
                 <ul className={styles.write_list}>
@@ -50,7 +57,10 @@ export default function WriteComponent() {
                     </li>
                     <li>
                         <div className={styles.write_txt}>내용</div>
-                        <WriteContent editorRef={editorRef} initialContent={post?.content || "내용을 입력해주세요."} />
+                        <WriteContent
+                            editorRef={editorRef}
+                            initialContent={post?.content || "내용을 입력해주세요."}
+                        />
                     </li>
                     <li>
                         <div className={styles.btn_box}>
